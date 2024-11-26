7d04f79d50ccd382f30ddba52d0069afc8f42277
sel <laoselwyn007@gmail.com>
2024-11-27 02:10:07 +0800

    Initial commit

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public void OnInteract()
+    {
+        Debug.Log("GO TO NEW AREA");
+        // Add functionality to navigate or load new areas
+    }
+}
